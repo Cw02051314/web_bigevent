b60777b19fa0b3cebb60aae870328a2affa92c9c
laochen <1374500896@qq.com>
2020-12-30 08:41:46 +0800

    feat:新增并完善了info页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -16,5 +16,5 @@
                 location.href = '/login.html'
             }
         }
-        
+
     })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,32 @@
+$(function () {
+    getUserInfo();
+    var layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+})
+
 function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
         // headers 就是请求头配置对象
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: function (res) {
             console.log(res);
             if (res.status !== 0) {
@@ -38,21 +59,3 @@ function renderAvatar(user) {
             .show()
     }
 }
-
-var layer = layui.layer;
-$('#btnLogout').on('click', function () {
-    // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', {
-        icon: 3,
-        title: '提示'
-    }, function (index) {
-        //do something
-        // 1. 清空本地存储中的 token
-        localStorage.removeItem('token')
-        // 2. 重新跳转到登录页面
-        location.href = '/login.html'
-        
-        // 关闭 confirm 询问框
-        layer.close(index)
-    })
-})
